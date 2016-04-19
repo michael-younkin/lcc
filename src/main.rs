1d91f75cc62596210a65ffc7173fbe46df524f90
Michael Younkin <younkin.michael@gmail.com>
2016-04-19 00:47:55 -0400

    Impl a messy messy (but simpler) parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, Read};
 use std::fmt;
 use std::fmt::{Display, Formatter};
+use std::mem;
 
 #[derive(Debug)]
 enum Token<'t> {
@@ -50,13 +51,24 @@ fn tokenize<'t>(input: &'t str) -> Result<Vec<Token<'t>>, &'static str> {
 macro_rules! consume {
     ($iter:expr, $pat:pat) => {
         if let Some($pat) = $iter.next() {
-            ()
+            Ok(())
         } else {
-            return Err(concat!("Expected ", stringify!($pat), "."))
+            Err(concat!("Expected ", stringify!($pat), "."))
         }
     }
 }
 
+macro_rules! consume_value {
+    ($iter:expr, $pat:pat, $m:ident) => {
+        if let Some($pat) = $iter.next() {
+            Ok($m)
+        } else {
+            Err(concat!("Expected ", stringify!($pat), "."))
+        }
+    }
+}
+
+#[derive(Debug)]
 enum Expr<'t> {
     Var(&'t str),
     Func(&'t str, Vec<Expr<'t>>),
@@ -64,6 +76,59 @@ enum Expr<'t> {
 }
 
 impl<'t> Expr<'t> {
+    fn with_input(input: &'t str) -> Result<Expr<'t>, &'static str> {
+        fn match_func<'t>(func: (&'t str, Vec<Expr<'t>>)) -> Expr<'t> {
+            match func {
+                ("", body) => Expr::Scope(body),
+                (arg, body) => Expr::Func(arg, body),
+            }
+        }
+        let mut tokens = try!(tokenize(input)).into_iter();
+        let mut scope_stack = Vec::new();
+        let mut current_func = ("", Vec::new());
+        while let Some(token) = tokens.next() {
+            match token {
+                Token::Var(s) => current_func.1.push(Expr::Var(s)),
+                Token::FuncStart => {
+                    let arg_name = try!(consume_value!(tokens, Token::Var(s), s));
+                    try!(consume!(tokens, Token::FuncDecEnd));
+                    let mut temp = (arg_name, Vec::new());
+                    mem::swap(&mut current_func, &mut temp);
+                    scope_stack.push(temp);
+                },
+                Token::LParen => {
+                    let mut temp = ("", Vec::new());
+                    mem::swap(&mut current_func, &mut temp);
+                    scope_stack.push(temp);
+                },
+                Token::RParen => {
+                    let expr = match_func(current_func);
+                    if let Some((arg, mut body)) = scope_stack.pop() {
+                        body.push(expr);
+                        current_func = (arg, body);
+                    } else {
+                        return Err("Extra right parenthese encountered.");
+                    }
+                },
+                _ => return Err("Unexpected function declaration ending.")
+            }
+        }
+        while scope_stack.len() > 0 {
+            let expr = match_func(current_func);
+            if let Some((arg, mut body)) = scope_stack.pop() {
+                body.push(expr);
+                current_func = (arg, body);
+            } else {
+                return Err("Extra right parenthese encountered.");
+            }
+        }
+        if let ("", body) = current_func {
+            return Ok(Expr::Scope(body))
+        } else {
+            panic!("Named scope at top of stack.");
+        }
+    }
+
     fn parenthesize_into(&self, buf: &mut String) {
         match *self {
             Expr::Var(ref name) => buf.push_str(name),
@@ -94,5 +159,5 @@ fn parenthesize_vec<'t>(buf: &mut String, vec: &Vec<Expr<'t>>) {
 }
 
 fn main() {
-    println!("{:?}", tokenize("λab.a (b c) a λc.d"));
+    println!("{:?}", Expr::with_input("λab.a (b c) a λc.d").unwrap());
 }
